8ce302e8d98254b7a3a9cb4c768bfbdb4f47942a
ur7ez <ur7ez@ukr.net>
2023-08-27 17:26:00 +0300

    add Vue components for alert and notification

---
+++ vue/src/components/Alert.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded text-sm">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style></style>
