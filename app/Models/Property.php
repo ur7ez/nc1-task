5257e92755788e76d148997417e610c38d2872b3
ur7ez <ur7ez@ukr.net>
2023-08-27 16:57:02 +0300

    created Property model and DB seeder for initial data

---
+++ app/Models/Property.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * @mixin Builder
+ */
+class Property extends ModelHelper
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'bedrooms',
+        'bathrooms',
+        'storeys',
+        'garages',
+    ];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Property;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,29 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Get the path to the CSV file
+        $csvFile = __DIR__ . '/data.csv';
+        if (file_exists($csvFile) && ($fileData = file($csvFile))) {
+            // read the CSV file into array
+            $csvData = array_map('str_getcsv', $fileData);
+            // remove the header (first row) from the CSV data
+            $header = array_shift($csvData);
+            // and seed the 'property' table
+            foreach ($csvData as $row) {
+                Property::create([
+                    'name' => $row[0],
+                    'price' => $row[1],
+                    'bedrooms' => $row[2],
+                    'bathrooms' => $row[3],
+                    'storeys' => $row[4],
+                    'garages' => $row[5],
+                ]);
+            }
+        } else {
+            // fill some data with factory
+            Property::factory(10)->create();
+        }
+
     }
 }
